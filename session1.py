e4d1c33fe6c3cb42d29fb85cc2a5820b6c2f4733
msa <msai.sadehi@gmail.com>
2023-07-04 22:57:01 +0330

    sadeghi

--- session1.py
+++ session1.py
@@ -1,1 +1,72 @@
-print("hello everybody")
+# print("hello everybody")
+# print(2 * 3.57)
+
+# *   /    +   -
+
+
+# print(2 + 3 * 2)
+# print((2 + 3) * 2)
+
+# score = 10
+
+# print("score is :", score)
+
+
+# x = 12
+# y = 5
+# z = 2
+
+
+# result = x * y - z
+
+# print("result is :", result)
+
+# a = 12
+# print(type(a))
+
+# b = 12.6
+# print(type(b))
+
+# name = "hello every body. welcome to python class."
+# print(name)
+# print(type(name))
+
+
+# message = "hello every body. \nwelcome to python class."
+# print(message)
+# message = """hello every body.
+# welcome to python class."""
+# print(message)
+# message = '''hello every body.
+# welcome to python class.'''
+# print(message)
+
+
+# message = "he said \"some thing\""
+# print(message)
+# message = 'he said "some thing"'
+# print(message)
+
+# name = input("enter a name: ")
+# course = input("enter the course name: ")
+# message = f"Hello {name}, welcome to {course} class."
+# print(message)
+
+
+# name = input("Enter your name: ")
+# family = input("Enter your family: ")
+
+
+# print(f"your name is:{name} and\nyour family is:{family}")
+
+
+# name = input("Enter your name: ")
+# print(type(name))
+
+# message = "your name is: " + name
+
+# print(message)
+
+# message = "hi "
+
+# print(message * 5)
