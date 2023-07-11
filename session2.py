e224f38edb54af013475f17dda90e895f1e527e7
msa <msai.sadehi@gmail.com>
2023-07-11 22:58:05 +0330

    sadeghi

---
+++ session2.py
@@ -0,0 +1,126 @@
+shopping_list = ["apple", "pen", "phone"]
+# print(shopping_list[0])
+# print(shopping_list[1])
+# print(shopping_list[2])
+
+# new_item  = input("enter an item: ")
+# shopping_list.append(new_item)
+# print("my shopping list is:", shopping_list)
+
+# del shopping_list[2]
+# print(shopping_list)
+# shopping_list.remove("pen")
+# print(shopping_list)
+# print(len(shopping_list))
+
+
+# numbers = [1,2,3,4,5,90,9,0]
+# print(numbers[0])
+# print(numbers[1])
+# print(numbers[2])
+# print(numbers[0] + numbers[1]+ numbers[2])
+
+# print(numbers[:3])
+# print(numbers[2:5])
+
+
+# string1 = "blallalalla"
+# string2 = "rrrrrrrrrrrrrrr"
+
+# print(string1 + string2)
+
+
+# list1 = [1,2,3]
+# list2 = [4,5,6]
+
+# print(list1 + list2)
+
+
+# mylist = [1, "reza", "a", [1, 2, 3]]
+# print(mylist)
+# print(mylist[0])
+# print(mylist[1])
+# print(mylist[2])
+# print(mylist[3])
+# print(mylist[3][0])
+# print(mylist[3][1])
+# print(mylist[3][2])
+
+
+# numbers = [1,2,3]
+# numbers[0] = 100
+# print(numbers)
+
+# numbers = (1, 2, 3)
+# numbers[0] = 100  # Error
+# favorite_sports = ["football", "tennis", "baseball"]
+
+# favorite_sports = {
+#     "artin": "football",
+#     "sara": "tennis",
+#     "reza": "baseball"
+# }
+
+# print(favorite_sports)
+# print(f"artin likes {favorite_sports['artin']}")
+# print(f"sara likes {favorite_sports['sara']}")
+# print(f"reza likes {favorite_sports['reza']}")
+
+# favorite_sports["armin"] = "football"
+# print(favorite_sports)
+
+# del favorite_sports["reza"]
+# print(favorite_sports)
+
+# score_1 = float(input("enter first score: "))
+# score_2 = float(input("enter second score: "))
+# score_3 = float(input("enter third score: "))
+
+# average = (score_1 + score_2 + score_3) /3
+
+# print(f"student's average is {average}")
+
+
+# number1 = int(input("enter a number: "))
+# number2 = int(input("enter a number: "))
+
+# print(number1/number2)
+
+# a = 4
+# b = 5
+
+# if a == b:
+#     print("a is equal to b")
+# if a != b:
+#     print("a is not equal to b")
+
+# if a > b:
+#     print("a is greater than b")
+# if a < b:
+#     print("a is less than b")
+
+
+# number1 = 12
+# number2 = 13
+
+# if number1 > number2:
+#     print("number1 is greater")
+
+# else:
+#     print("number2 is greater")
+
+
+a = 4
+b = 7
+c = 5
+
+if a > b:
+    print("a > b")
+elif b > c:
+    print("b > c")
+
+elif a > c:
+    print("a > c")
+
+else:
+    print("some thing")
